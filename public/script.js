01ec7bdc14d8daa6705658e35859449472cfe2cf
UbaidRussell <UbaidRussell@users.noreply.github.com>
2024-08-22 04:39:16 -0400

    Added script.js

---
+++ public/script.js
@@ -0,0 +1,28 @@
+const tmdbKey = '74890179d22c12692a9dbd9a72bdf43d';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3/movie/550?api_key=';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres = () => {
+
+};
+
+const getMovies = () => {
+  const selectedGenre = getSelectedGenre();
+
+};
+
+const getMovieInfo = () => {
+
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
